ffcde7d2e8fe942b30607f439befcc6791f9725a
alexsharich <alexsharich@gmail.com>
2023-01-20 15:23:42 +0100

    add parallax slider

---
+++ js/app.js
@@ -0,0 +1,11 @@
+new Swiper ('.slider',{
+  //direction:'vertical',
+  freeMode:true,
+  speed:2500,
+  mousewheel:{
+    enables:true,
+    sensitivity:2.5
+  },
+  spaceBetween:18,
+  parallax:true,
+})
